1c62b9d8fec1e333f1a1089d53f3e3107e1430e7
Denise Tabilas <tabilas.denise@gmail.com>
2022-10-15 00:27:52 -0700

    moved files

---
+++ Passenger Project/Project5.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEditPassenger
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtSeat = New System.Windows.Forms.TextBox()
+        Me.lblSeat = New System.Windows.Forms.Label()
+        Me.lblSeatExample = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnList = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.lstWaitlist = New System.Windows.Forms.ListBox()
+        Me.lblWaitlist = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Century Schoolbook", 20.0!, System.Drawing.FontStyle.Bold)
+        Me.lblName.Location = New System.Drawing.Point(32, 42)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(204, 64)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Name:"
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Century Schoolbook", 15.0!)
+        Me.txtName.Location = New System.Drawing.Point(242, 50)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(511, 56)
+        Me.txtName.TabIndex = 1
+        '
+        'txtSeat
+        '
+        Me.txtSeat.Font = New System.Drawing.Font("Century Schoolbook", 15.0!)
+        Me.txtSeat.Location = New System.Drawing.Point(969, 50)
+        Me.txtSeat.Name = "txtSeat"
+        Me.txtSeat.Size = New System.Drawing.Size(330, 56)
+        Me.txtSeat.TabIndex = 3
+        '
+        'lblSeat
+        '
+        Me.lblSeat.AutoSize = True
+        Me.lblSeat.Font = New System.Drawing.Font("Century Schoolbook", 20.0!, System.Drawing.FontStyle.Bold)
+        Me.lblSeat.Location = New System.Drawing.Point(781, 42)
+        Me.lblSeat.Name = "lblSeat"
+        Me.lblSeat.Size = New System.Drawing.Size(166, 64)
+        Me.lblSeat.TabIndex = 2
+        Me.lblSeat.Text = "Seat:"
+        '
+        'lblSeatExample
+        '
+        Me.lblSeatExample.AutoSize = True
+        Me.lblSeatExample.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSeatExample.Location = New System.Drawing.Point(962, 124)
+        Me.lblSeatExample.Name = "lblSeatExample"
+        Me.lblSeatExample.Size = New System.Drawing.Size(315, 38)
+        Me.lblSeatExample.TabIndex = 4
+        Me.lblSeatExample.Text = "(1A, 1B -- 10A, 10B)"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Century Schoolbook", 15.0!, System.Drawing.FontStyle.Bold)
+        Me.btnAdd.Location = New System.Drawing.Point(43, 213)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(696, 148)
+        Me.btnAdd.TabIndex = 5
+        Me.btnAdd.Text = "Add Passenger"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Century Schoolbook", 15.0!, System.Drawing.FontStyle.Bold)
+        Me.btnDelete.Location = New System.Drawing.Point(43, 396)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(696, 148)
+        Me.btnDelete.TabIndex = 6
+        Me.btnDelete.Text = "Delete Passenger"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Font = New System.Drawing.Font("Century Schoolbook", 15.0!, System.Drawing.FontStyle.Bold)
+        Me.btnReset.Location = New System.Drawing.Point(43, 762)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(696, 148)
+        Me.btnReset.TabIndex = 8
+        Me.btnReset.Text = "Reset All"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnList
+        '
+        Me.btnList.Font = New System.Drawing.Font("Century Schoolbook", 15.0!, System.Drawing.FontStyle.Bold)
+        Me.btnList.Location = New System.Drawing.Point(43, 575)
+        Me.btnList.Name = "btnList"
+        Me.btnList.Size = New System.Drawing.Size(696, 148)
+        Me.btnList.TabIndex = 7
+        Me.btnList.Text = "List Passengers"
+        Me.btnList.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Font = New System.Drawing.Font("Century Schoolbook", 15.0!, System.Drawing.FontStyle.Bold)
+        Me.btnClose.Location = New System.Drawing.Point(43, 957)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(696, 148)
+        Me.btnClose.TabIndex = 9
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Font = New System.Drawing.Font("Courier New", 15.0!, System.Drawing.FontStyle.Bold)
+        Me.txtOutput.Location = New System.Drawing.Point(792, 213)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(519, 521)
+        Me.txtOutput.TabIndex = 10
+        '
+        'lblCount
+        '
+        Me.lblCount.AutoSize = True
+        Me.lblCount.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Italic)
+        Me.lblCount.Location = New System.Drawing.Point(997, 747)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(280, 38)
+        Me.lblCount.TabIndex = 11
+        Me.lblCount.Text = "Passenger Count: "
+        '
+        'lstWaitlist
+        '
+        Me.lstWaitlist.Font = New System.Drawing.Font("Century Schoolbook", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.lstWaitlist.FormattingEnabled = True
+        Me.lstWaitlist.ItemHeight = 32
+        Me.lstWaitlist.Location = New System.Drawing.Point(792, 877)
+        Me.lstWaitlist.Name = "lstWaitlist"
+        Me.lstWaitlist.Size = New System.Drawing.Size(519, 228)
+        Me.lstWaitlist.TabIndex = 12
+        '
+        'lblWaitlist
+        '
+        Me.lblWaitlist.AutoSize = True
+        Me.lblWaitlist.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Italic)
+        Me.lblWaitlist.Location = New System.Drawing.Point(785, 836)
+        Me.lblWaitlist.Name = "lblWaitlist"
+        Me.lblWaitlist.Size = New System.Drawing.Size(143, 38)
+        Me.lblWaitlist.TabIndex = 13
+        Me.lblWaitlist.Text = "Waitlist:"
+        '
+        'frmEditPassenger
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(1385, 1166)
+        Me.Controls.Add(Me.lblWaitlist)
+        Me.Controls.Add(Me.lstWaitlist)
+        Me.Controls.Add(Me.lblCount)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnList)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lblSeatExample)
+        Me.Controls.Add(Me.txtSeat)
+        Me.Controls.Add(Me.lblSeat)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "frmEditPassenger"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Project5 - Passengers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtSeat As TextBox
+    Friend WithEvents lblSeat As Label
+    Friend WithEvents lblSeatExample As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnList As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents lblCount As Label
+    Friend WithEvents lstWaitlist As ListBox
+    Friend WithEvents lblWaitlist As Label
+End Class
---
+++ Passenger Project/Project5.vb
@@ -0,0 +1,273 @@
+﻿
+Public Class frmEditPassenger
+  Dim arystrPassengers(10, 4) As String
+  Dim intWaitlistCount As Integer = 0
+  'row #0 and col#0 for total
+  Dim intCount As Integer = 0
+  Dim intCapacity As Integer = 40
+
+  Private Sub frmEditPassenger_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    For intRow As Integer = 0 To 10
+      For intCol As Integer = 0 To 4
+        arystrPassengers(intRow, intCol) = "0"
+      Next
+    Next
+    DisplayChart()
+
+  End Sub
+  Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+    'Add a passenger to list if there is an open seat
+    'if no seat availabe, add to waitlist
+
+    'validate if there is a name
+    If txtName.Text.Length = 0 Then
+      MessageBox.Show("Must enter a name")
+      Return
+    End If
+
+    'check if passenger is already seated
+    If SearchName(txtName.Text) = True Then 'name is found in list
+      MessageBox.Show("Passenger '" & txtName.Text & "' is already seated")
+      Return
+    End If
+
+    'If reached max capacity, add to waitlist
+    If intCount = intCapacity Then
+      lstWaitlist.Items.Add(txtName.Text)
+      intWaitlistCount += 1
+      Return
+    End If
+
+    'If seat is specified, validate seat
+    If txtSeat.Text.Length <> 0 Then
+      If ValidateSeat(txtSeat.Text) Then
+        'check if seat is vacant
+        Dim aryintPos() As Integer = TranslateSeat(txtSeat.Text) 'MessageBox.Show(CStr(aryintPos(0)) & " " + CStr(aryintPos(1)))
+        If isVacant(aryintPos(0), aryintPos(1)) = True Then
+          'insert reservation
+          AddPassenger(aryintPos(0), aryintPos(1), txtName.Text)
+          DisplayChart()
+          Return
+        Else
+          MessageBox.Show("Seat is occupied")
+          Return
+        End If
+      Else
+        MessageBox.Show("Invalid Seat Format")
+        Return
+      End If
+    End If
+
+    'seat is not specified, add to any open seat
+    FindVacantSeat(txtName.Text) 'TEST
+  End Sub
+
+  Function SearchName(strName As String)
+    'return true if name is found and display seat, else return false
+    For intRow As Integer = 1 To 10
+      If CInt(arystrPassengers(intRow, 0)) > 0 Then 'row is not empty
+        For intCol As Integer = 1 To 4
+          If arystrPassengers(intRow, intCol) = strName Then
+            txtSeat.Text = TranslateSeatPosition(intRow, intCol)
+            Return True
+          End If
+        Next
+      End If
+    Next
+    Return False
+  End Function
+
+  Private Sub FindVacantSeat(strName As String)
+    For intRow As Integer = 1 To 10
+      If CInt(arystrPassengers(intRow, 0)) < 4 Then 'if row is not full
+        For intCol As Integer = 1 To 4
+          If isVacant(intRow, intCol) = True Then 'found vacancy
+            AddPassenger(intRow, intCol, strName)
+            txtSeat.Text = TranslateSeatPosition(intRow, intCol)
+            DisplayChart()
+            Return
+          End If
+        Next
+      End If
+    Next
+  End Sub
+  Function ValidateSeat(strSeat As String)
+    'check if strSeat is valid
+
+    'check if there are 2 characters in string 
+    If strSeat.Length() > 3 Or strSeat.Length < 2 Then
+      Return False
+    End If
+
+    Dim strFirst As String = ""
+    Dim strSec As String = ""
+    If strSeat.Length() = 2 Then
+      strFirst = strSeat.Substring(0, 1)
+      strSec = strSeat.Substring(1, 1).ToUpper()
+    Else 'length = 3
+      strFirst = strSeat.Substring(0, 2)
+      strSec = strSeat.Substring(2, 1).ToUpper()
+    End If
+
+    'check if strFirst is numeric and is 1-10
+    If IsNumeric(strFirst) = False Then
+      Return False
+    Else
+      Dim intNum As Integer = CInt(strFirst)
+      If intNum < 1 Or intNum > 10 Then
+        Return False
+      End If
+    End If
+
+    'check if strSecond is a letter and is A-D
+    If IsNumeric(strSec) = True Then
+      Return False
+    Else
+      If strSec < "A" Or strSec > "D" Then
+        Return False
+      End If
+    End If
+    Return True
+  End Function
+  Function TranslateSeat(strSeat As String)
+    'translate strSeat to position in 2d array
+    Dim aryintSeatPos(1) As Integer
+    Dim strSec As Char 'strSeat.Substring(1, 1).ToUpper
+    If strSeat.Length() = 2 Then
+      aryintSeatPos(0) = CInt(strSeat.Substring(0, 1))
+      strSec = strSeat.Substring(1, 1).ToUpper()
+    Else
+      aryintSeatPos(0) = 10
+      strSec = strSeat.Substring(2, 1).ToUpper()
+    End If
+    Select Case strSec
+      Case "A"
+        aryintSeatPos(1) = 1
+      Case "B"
+        aryintSeatPos(1) = 2
+      Case "C"
+        aryintSeatPos(1) = 3
+      Case Else
+        aryintSeatPos(1) = 4
+    End Select
+    Return aryintSeatPos
+  End Function
+  Function isVacant(intX As Integer, intY As Integer)
+    If arystrPassengers(intX, intY) = "0" Then
+      Return True
+    End If
+    Return False
+  End Function
+  Private Sub AddPassenger(intX As Integer, intY As Integer, strName As String)
+    arystrPassengers(intX, intY) = strName
+    arystrPassengers(intX, 0) = CInt(arystrPassengers(intX, 0)) + 1
+    arystrPassengers(0, intY) = CInt(arystrPassengers(0, intY)) + 1
+    intCount += 1
+  End Sub
+  Private Sub DisplayChart()
+    txtOutput.Text = "Row  A  B    C  D" & vbCrLf
+    Dim fmtStr As String = "{0, 2}{1, 4}{2, 3}{3, 5}{4,3}"
+    For intRow As Integer = 1 To 10
+      Dim strRowNum As String = CStr(intRow)
+      Dim data(3) As String
+      For intCol As Integer = 1 To 4
+        If isVacant(intRow, intCol) = False Then
+          data(intCol - 1) = "X"
+        Else
+          data(intCol - 1) = " "
+        End If
+      Next
+      txtOutput.Text += String.Format(fmtStr, strRowNum, data(0), data(1), data(2), data(3))
+      txtOutput.Text += vbCrLf
+    Next
+    lblCount.Text = "Passenger Count: " & CStr(intCount)
+  End Sub
+  Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+    'Deletes passenger's resservation given their name 
+    If txtName.TextLength() = 0 Then
+      MessageBox.Show("Must enter name to delete")
+      Return
+    End If
+
+    'loop through passenger array to search for name, if found, delete
+    For intRow As Integer = 1 To 10
+      If CInt(arystrPassengers(intRow, 0)) > 0 Then 'if row is not empty
+        For intCol As Integer = 1 To 4
+          If arystrPassengers(intRow, intCol) = txtName.Text Then 'found
+            DeletePassenger(intRow, intCol)
+            DisplayChart()
+            Return
+          End If
+        Next
+      End If
+    Next
+    'not found
+    MessageBox.Show("Passenger '" & txtName.Text & "' not found")
+  End Sub
+  Private Sub DeletePassenger(intX, intY)
+    arystrPassengers(intX, intY) = "0"
+    intCount -= 1
+    arystrPassengers(intX, 0) = CInt(arystrPassengers(intX, 0)) - 1
+    arystrPassengers(0, intY) = CInt(arystrPassengers(0, intY)) - 1
+
+    If intWaitlistCount > 0 Then 'if thereis someone on waitlist, add first to deleted position
+      Dim strName As String = lstWaitlist.Items(0)
+      lstWaitlist.Items.RemoveAt(0)
+      intWaitlistCount -= 1
+      AddPassenger(intX, intY, strName)
+    End If
+
+  End Sub
+  Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+    Me.Close()
+  End Sub
+  Private Sub btnList_Click(sender As Object, e As EventArgs) Handles btnList.Click
+    Dim strText As String = ""
+    strText = "#:  Passenger Name:          Seat:" & vbCrLf
+    Dim fmtStr As String = "{0, 2}{1, 23}{2, 7}"
+    Dim intNum As Integer = 1
+    For intRow As Integer = 1 To 10
+      For intCol As Integer = 1 To 4
+        Dim strName As String = arystrPassengers(intRow, intCol)
+        If strName = "0" Then
+          strName = "*** Available *******"
+        End If
+        strText += String.Format(fmtStr, CStr(intNum),
+        strName, TranslateSeatPosition(intRow, intCol)) &
+        vbCrLf
+        intNum += 1
+      Next
+    Next
+
+    Dim frmDisplay As New frmDisplay
+    frmDisplay.txtDocument.Text = strText
+    frmDisplay.txtDocument.SelectionStart = 0
+    frmDisplay.ShowDialog()
+
+  End Sub
+  Function TranslateSeatPosition(intX As Integer, intY As Integer)
+    Dim strResult As String = ""
+    strResult += CStr(intX)
+    Select Case intY
+      Case 1
+        strResult += "A"
+      Case 2
+        strResult += "B"
+      Case 3
+        strResult += "C"
+      Case Else
+        strResult += "D"
+    End Select
+    Return strResult
+  End Function
+
+  Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+    For intRow As Integer = 0 To 10
+      For intCol As Integer = 0 To 4
+        arystrPassengers(intRow, intCol) = "0"
+      Next
+    Next
+    intCount = 0
+    DisplayChart()
+  End Sub
+End Class
